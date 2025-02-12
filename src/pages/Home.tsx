d8dc70ff0c8d0b394d9d7287c541edf1b85eb3c3
njagua <njagua.gma@gmail.com>
2025-02-12 23:49:25 +0300

    remove github links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ export function Home() {
             description="An online reservation system designed to streamline booking and scheduling processes. The platform provides a user-friendly interface for managing reservations efficiently, ensuring a seamless experience for both businesses and customers."
             technologies={["Supabase", "Javascript", "Typescript", "Tailwind CSS", "React JS"]}
             liveUrl="https://reserveaseapp.netlify.app/"
-            githubUrl="#"
+          
             icon={<ShoppingBag className="w-6 h-6" />}
             image="https://images.unsplash.com/photo-1551632436-cbf8dd35adfa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjBkaW5pbmd8ZW58MHx8MHx8fDA%3D?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGFibGUlMjByZXNlcnZhdGlvbnxlbnwwfHwwfHx8MA%3D%3D?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2plY3QlMjBtYW5hZ2VtZW50fGVufDB8fDB8fHww?auto=format&fit=crop&w=800&q=80"
           />
@@ -38,7 +38,7 @@ export function Home() {
             description="A personal website showcasing Mary Nyingi`s skills, projects, and professional background. It highlights her expertise, experience, and creative work in a visually appealing and user-friendly manner."
             technologies={["Javascript", "Typescript", "Tailwind CSS", "Lucide Icons"]}
             liveUrl="https://marynyingi.netlify.app/"
-            githubUrl="#"
+            
             icon={<ShoppingBag className="w-6 h-6" />}
             image="https://images.unsplash.com/photo-1537462715879-360eeb61a0ad?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVjaGFuaWNhbCUyMGVuZ2luZWVyaW5nfGVufDB8fDB8fHww?auto=format&fit=crop&w=800&q=80"
           />
@@ -47,7 +47,7 @@ export function Home() {
             description="A comprehensive property management solution that streamlines tenant communication, maintenance requests, and rent collection. Features include automated billing, maintenance tracking, and detailed financial reporting."
             technologies={["React", "Javascript", "Unsplash", "Tailwind CSS"]}
             liveUrl="https://ringbeltagents.netlify.app/"
-            githubUrl="https://github.com/njagua3/Ringbelt_Agents"
+            //githubUrl="https://github.com/njagua3/Ringbelt_Agents"
             icon={<Building className="w-6 h-6" />}
             image="https://images.unsplash.com/photo-1560518883-ce09059eeffa?auto=format&fit=crop&w=800&q=80"
           />
@@ -56,7 +56,7 @@ export function Home() {
             description="A collaborative project management tool designed specifically for Moringa School, enabling students and instructors to track project progress, share resources, and manage deadlines effectively."
             technologies={["React", "Flask", "Javascript", "Material-UI", "Lucide Icons"]}
             liveUrl="https://moringaschool.netlify.app/"
-            githubUrl="#"
+            
             icon={<ClipboardList className="w-6 h-6" />}
             image="https://images.unsplash.com/photo-1507925921958-8a62f3d1a50d?auto=format&fit=crop&w=800&q=80"
           />
@@ -65,7 +65,7 @@ export function Home() {
             description="A seamless travel platform offering destination guides, travel tips, and essential information for exploring Kenya's culture effortlessly."
             technologies={["React", "Javascript", "Tailwind CSS", "Unsplash"]}
             liveUrl="https://safirike.netlify.app/"
-            githubUrl="#"
+            
             icon={<Package className="w-6 h-6" />}
             image="https://plus.unsplash.com/premium_photo-1664302700377-d544c98a8a51?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&w=800&q=80"
           />
