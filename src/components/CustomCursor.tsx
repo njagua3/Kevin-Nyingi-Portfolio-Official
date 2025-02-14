b58be221e266849ae94671e4825650a267f9c368
njagua <njagua.gma@gmail.com>
2025-02-14 23:06:13 +0300

    image load time

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function CustomCursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
